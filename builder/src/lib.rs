1032782853eb332e9dd4f18504e9e316d73dcc4f
José Duarte <jmg.duarte@campus.fct.unl.pt>
2020-12-02 20:07:06 +0000

    Pass test 01-parser

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,10 +1,11 @@
-extern crate proc_macro;
+// extern crate proc_macro;
 
 use proc_macro::TokenStream;
+use syn::{DeriveInput, parse_macro_input};
+use quote::quote;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+    let input = parse_macro_input!(input as DeriveInput);
+    TokenStream::from(quote!{})
 }
