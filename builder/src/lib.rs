21ad770926d4f6d3dda0eba05d2feaca6fd01655
José Duarte <jmg.duarte@campus.fct.unl.pt>
2020-12-03 18:11:49 +0000

    Add missing `Command::builder()` implementation

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -9,7 +9,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     println!("{:#?}", input);
     let struct_ident_original = input.ident;
-    let original_struct_impl = impl_struct(&struct_ident_original);
+    let original_struct_impl = impl_struct(&struct_ident_original, &input.data).unwrap();
     let builder_struct = builder_struct(&struct_ident_original, &input.data).unwrap();
 
     quote!(
@@ -19,12 +19,30 @@ pub fn derive(input: TokenStream) -> TokenStream {
     .into()
 }
 
-fn impl_struct(struct_ident: &syn::Ident) -> proc_macro2::TokenStream {
-    quote! (
-        impl #struct_ident {
-            pub fn builder() {}
-        }
-    )
+fn impl_struct(struct_ident: &syn::Ident, data: &syn::Data) -> Option<proc_macro2::TokenStream> {
+    if let syn::Data::Struct(syn::DataStruct {
+        fields:
+            syn::Fields::Named(syn::FieldsNamed {
+                named: named_fields,
+                ..
+            }),
+        ..
+    }) = data
+    {
+        let builder_struct_inits = named_fields.iter().map(initialize_field);
+        let builder_struct_ident = format_ident!("{}Builder", struct_ident);
+        Some(quote! (
+            impl #struct_ident {
+                pub fn builder() -> #builder_struct_ident {
+                    #builder_struct_ident {
+                        #(#builder_struct_inits),*
+                    }
+                }
+            }
+        ))
+    } else {
+        None
+    }
 }
 
 fn builder_struct(struct_ident: &syn::Ident, data: &syn::Data) -> Option<proc_macro2::TokenStream> {
@@ -50,6 +68,11 @@ fn builder_struct(struct_ident: &syn::Ident, data: &syn::Data) -> Option<proc_ma
     }
 }
 
+
+fn initialize_field(field: &syn::Field) -> proc_macro2::TokenStream {
+    let ref field_name = field.ident;
+    quote!(#field_name: None)
+}
 fn optionize_field(field: &syn::Field) -> proc_macro2::TokenStream {
     let ref field_name = field.ident;
     let ref field_type = field.ty;
