dfc9ebbf1a8b9c350e7cf6180a06bfebb3b43a8a
José Duarte <jmg.duarte@campus.fct.unl.pt>
2020-12-04 19:05:00 +0000

    Implement a base for 07 - really needs rewriting

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -4,7 +4,7 @@ use proc_macro::TokenStream;
 use quote::{format_ident, quote};
 use syn::{parse_macro_input, DeriveInput};
 
-#[proc_macro_derive(Builder)]
+#[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let struct_ident_original = input.ident;
@@ -102,17 +102,79 @@ fn assign_field(field: &syn::Field) -> proc_macro2::TokenStream {
 }
 
 fn functionize_field(field: &syn::Field) -> proc_macro2::TokenStream {
-    let ref field_name = field.ident;
+    let field_name = field.ident.as_ref().unwrap();
     let mut field_type = &field.ty;
     if let Some(inner_ty) = extract_inner_type(field_type, "Option") {
         field_type = inner_ty;
     }
+    let once_setter_tt = once_setter(field_name, field_type);
+    if field.attrs.is_empty() {
+        return quote!(
+           #once_setter_tt
+        );
+    }
+    let attr_value = &format_ident!(
+        "{}",
+        extract_each_attr_value(&field.attrs).expect("invalid use of builder attr")
+    );
+    let vec_inner_type =
+        extract_inner_type(field_type, "Vec").expect("inner type of Vec is <>????");
+    let each_setter_tt = each_setter(attr_value, field_name, vec_inner_type);
+    if attr_value == field_name {
+        quote!(
+            #each_setter_tt
+        )
+    } else {
+        quote! (
+            #once_setter_tt
+            #each_setter_tt
+        )
+    }
+}
+
+fn once_setter(field_name: &syn::Ident, field_type: &syn::Type) -> proc_macro2::TokenStream {
+    quote!(fn #field_name(&mut self, #field_name: #field_type) -> &mut Self {
+        self.#field_name = Some(#field_name);
+        self
+    })
+}
+fn each_setter(
+    method_name: &syn::Ident,
+    field_name: &syn::Ident,
+    field_type: &syn::Type,
+) -> proc_macro2::TokenStream {
     quote!(
-        fn #field_name(&mut self, #field_name: #field_type) -> &mut Self {
-            self.#field_name = Some(#field_name);
+        fn #method_name(&mut self, #field_name: #field_type) -> &mut Self {
+            if let Some(ref mut v) = self.#field_name {
+                v.push(#field_name);
+            } else {
+                self.#field_name = Some(vec![#field_name]);
+            }
             self
         }
     )
+}
+
+fn extract_each_attr_value(attrs: &Vec<syn::Attribute>) -> Option<String> {
+    if !attrs.is_empty() {
+        if let Some(attr) = attrs.last() {
+            if let Ok(syn::Meta::List(list)) = attr.parse_meta() {
+                if list.path.is_ident("builder") {
+                    if let Some(syn::NestedMeta::Meta(syn::Meta::NameValue(syn::MetaNameValue {
+                        path,
+                        lit: syn::Lit::Str(lit_str),
+                        ..
+                    }))) = list.nested.last()
+                    {
+                        if path.is_ident("each") {
+                            return Some(lit_str.value());
+                        }
+                    }
+                }
+            }
+        }
+    }
+    None
 }
 
 fn optionize_field(field: &syn::Field) -> proc_macro2::TokenStream {
