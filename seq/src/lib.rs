9eccae6922640b786b402cf8e8900396553679f6
José Duarte <jmg.duarte@campus.fct.unl.pt>
2020-12-10 18:00:04 +0000

    [Seq] Pass 01-parse-headers

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,10 +1,31 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{Token, braced, parse::Parse, parse_macro_input};
+
+struct Seq {
+    name: syn::Ident,
+    start: syn::Lit,
+    end: syn::Lit,
+}
+
+impl Parse for Seq {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let content;
+        let name = input.parse()?;
+        input.parse::<Token![in]>()?;
+        let start = input.parse()?;
+        input.parse::<Token![..]>()?;
+        let end = input.parse()?;
+        braced!(content in input);
+        Ok(Seq { name, start, end})
+    }
+}
 
 #[proc_macro]
 pub fn seq(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let Seq { name, start, end } = parse_macro_input!(input as Seq);
 
-    unimplemented!()
+    quote!().into()
 }
