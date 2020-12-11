a3b05bdb418ce6eee453be3b72dc2c30490e28d0
José Duarte <jmg.duarte@campus.fct.unl.pt>
2020-12-11 11:57:18 +0000

    [Sorted] Pass 01-parse-enum

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,11 +1,15 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::parse_macro_input;
 
 #[proc_macro_attribute]
 pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
     let _ = args;
-    let _ = input;
+    let input = parse_macro_input!(input as syn::Item);
 
-    unimplemented!()
+    println!("{:#?}", input);
+
+    quote!(#input).into()
 }
