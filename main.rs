b4889ea593dd1a72bf5e959c53fdfc0e8db63c94
José Duarte <jmg.duarte@campus.fct.unl.pt>
2020-12-14 19:03:44 +0000

    Refactor some code in way for 05

--- main.rs
+++ main.rs
@@ -8,23 +8,14 @@
 
 use seq::seq;
 
-seq!(N in 1..4 {
-    fn f#N () -> u64 {
-        N * 2
-    }
+macro_rules! expand_to_nothing {
+    ($arg:literal) => {
+        // nothing
+    };
+}
+
+seq!(N in 0..4 {
+    expand_to_nothing!(N);
 });
 
-// This f0 is written separately to detect whether your macro correctly starts
-// with the first iteration at N=1 as specified in the invocation. If the macro
-// incorrectly started at N=0 like in the previous tests cases, the first
-// generated function would conflict with this one and the program would not
-// compile.
-fn f0() -> u64 {
-    100
-}
-
-fn main() {
-    let sum = f0() + f1() + f2() + f3();
-
-    assert_eq!(sum, 100 + 2 + 4 + 6);
-}
+fn main() {}
--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -18,33 +18,80 @@ pub fn seq(input: TokenStream) -> TokenStream {
 #[derive(Debug)]
 struct Seq {
     var: syn::Ident,
-    start: syn::LitInt,
-    end: syn::LitInt,
+    start: i64,
+    end: i64,
     body: proc_macro2::TokenStream,
     inclusive: bool,
 }
 
 impl Seq {
     fn expand(&self) -> syn::Result<proc_macro2::TokenStream> {
-        let start: usize = self.start.base10_parse()?;
-        let end: usize = self.end.base10_parse()?;
         let mut output = proc_macro2::TokenStream::new();
         if self.inclusive {
-            for value in start..=end {
+            for value in self.start..=self.end {
                 output.extend(self.inner_expand(value, self.body.clone()));
             }
         } else {
-            for value in start..end {
+            for value in self.start..self.end {
                 output.extend(self.inner_expand(value, self.body.clone()));
             }
         }
+        // let output = self.expand_range(self.body.clone())?;
         println!("{:#?}", output);
         syn::Result::Ok(output)
     }
 
+    #[allow(dead_code)]
+    fn expand_range(
+        &self,
+        mut tokens: proc_macro2::TokenStream,
+    ) -> syn::Result<proc_macro2::TokenStream> {
+        if self.inclusive {
+            for value in self.start..=self.end {
+                tokens.extend(self.inner_expand(value, self.body.clone()));
+            }
+        } else {
+            for value in self.start..self.end {
+                tokens.extend(self.inner_expand(value, self.body.clone()));
+            }
+        }
+        syn::Result::Ok(tokens)
+    }
+
+    #[allow(dead_code)]
+    fn expand_repetitions(&self) -> syn::Result<proc_macro2::TokenStream> {
+        let mut tokens: Vec<proc_macro2::TokenTree> = self.body.clone().into_iter().collect();
+        let mut i = 0;
+        while i < tokens.len() - 2 {
+            match &mut tokens[i..i + 3] {
+                // check for a repetition group
+                // #(...)*
+                [proc_macro2::TokenTree::Punct(pound), proc_macro2::TokenTree::Group(group), proc_macro2::TokenTree::Punct(star)]
+                    if pound.as_char() == '#' && star.as_char() == '*' =>
+                {
+                    if let proc_macro2::Delimiter::Parenthesis = group.delimiter() {
+                        let original_span = group.span();
+                        *group = proc_macro2::Group::new(
+                            group.delimiter(),
+                            self.expand_range(group.stream())?,
+                        );
+                        group.set_span(original_span);
+                        i += 3;
+                        continue;
+                    }
+                }
+                _ => {
+                    i += 1;
+                    continue;
+                }
+            }
+        }
+        syn::Result::Ok(quote!(#(#tokens)*))
+    }
+
     fn inner_expand(
         &self,
-        value: usize,
+        value: i64,
         tokens: proc_macro2::TokenStream,
     ) -> syn::Result<proc_macro2::TokenStream> {
         let mut tokens: Vec<proc_macro2::TokenTree> = tokens.into_iter().collect();
@@ -66,15 +113,23 @@ impl Seq {
                 // skip processing of other possibilities
                 continue;
             }
+            // detected some form of punctuation
             if let proc_macro2::TokenTree::Punct(punct) = &mut tokens[i] {
+                // if it is not # we don't care for it
+                // so we just keep going
                 if punct.as_char() != '#' {
                     i += 1;
                     continue;
                 }
-                match (&tokens[i - 1], &tokens[i + 1]) {
-                    (proc_macro2::TokenTree::Ident(prefix), proc_macro2::TokenTree::Ident(var))
-                        if self.var.to_string() == var.to_string() =>
-                    {
+
+                // try to match the previous and next tokens
+                // if both tokens are idents
+                // and the next token is an equal ident to the seq var
+                // create a new token and replace the three tokens with the new ones
+                if let (proc_macro2::TokenTree::Ident(prefix), proc_macro2::TokenTree::Ident(var)) =
+                    (&tokens[i - 1], &tokens[i + 1])
+                {
+                    if self.var.to_string() == var.to_string() {
                         let ident = proc_macro2::Ident::new(
                             &format!("{}{}", prefix.to_string(), value),
                             prefix.span(),
@@ -84,16 +139,12 @@ impl Seq {
                         i += 2;
                         continue;
                     }
-                    _ => {
-                        i += 1;
-                        continue;
-                    }
                 }
             }
 
             if let proc_macro2::TokenTree::Ident(ident) = &mut tokens[i] {
                 if *ident == self.var {
-                    let mut lit = proc_macro2::Literal::usize_unsuffixed(value);
+                    let mut lit = proc_macro2::Literal::i64_unsuffixed(value);
                     lit.set_span(ident.span());
                     tokens[i] = proc_macro2::TokenTree::Literal(lit);
                 }
@@ -112,7 +163,8 @@ impl Parse for Seq {
     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
         let var = input.parse()?;
         input.parse::<Token![in]>()?;
-        let start = input.parse()?;
+        let start: syn::LitInt = input.parse()?;
+        let start: i64 = start.base10_parse()?;
         let inclusive = if input.peek(Token![..=]) {
             input.parse::<Token![..=]>()?;
             true
@@ -120,7 +172,8 @@ impl Parse for Seq {
             input.parse::<Token![..]>()?;
             false
         };
-        let end = input.parse()?;
+        let end: syn::LitInt = input.parse()?;
+        let end: i64 = end.base10_parse()?;
         let content;
         braced!(content in input);
         let body = content.parse()?;
