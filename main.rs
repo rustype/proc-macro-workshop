2a0fbc5a820a50e8a04fdb908652308697811cb2
José Duarte <jmg.duarte@campus.fct.unl.pt>
2020-12-14 14:59:47 +0000

    [Seq] Pass 03-expand-four-errors

--- main.rs
+++ main.rs
@@ -5,30 +5,10 @@
 //
 // To run the code:
 //     $ cargo run
+use seq::seq;
 
-use sorted::sorted;
-
-#[sorted]
-pub enum Conference {
-    RustBeltRust,
-    RustConf,
-    RustFest,
-    RustLatam,
-    RustRush,
-}
-
-impl Conference {
-    #[sorted::check]
-    pub fn region(&self) -> &str {
-        use self::Conference::*;
-
-        #[sorted]
-        match self {
-            RustFest => "Europe",
-            RustLatam => "Latin America",
-            _ => "elsewhere",
-        }
-    }
-}
+seq!(N in 0..4 {
+    compile_error!(concat!("error number ", stringify!(N)));
+});
 
 fn main() {}
--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,21 +1,94 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
-use quote::quote;
+use quote::{ quote};
 use syn::{braced, parse::Parse, parse_macro_input, Token};
+
+#[proc_macro]
+pub fn seq(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as Seq);
+    println!("{:#?}", input);
+    let output = input.expand();
+    println!("{:#?}", output);
+    match output {
+        Ok(ts) => ts.into(),
+        Err(e) => e.to_compile_error().into(),
+    }
+}
 
 #[derive(Debug)]
 struct Seq {
-    name: syn::Ident,
+    var: syn::Ident,
     start: syn::LitInt,
     end: syn::LitInt,
     body: proc_macro2::TokenStream,
     inclusive: bool,
 }
 
+impl Seq {
+    fn expand(&self) -> syn::Result<proc_macro2::TokenStream> {
+        let start: usize = self.start.base10_parse()?;
+        let end: usize = self.end.base10_parse()?;
+        println!("start:    {}", start);
+        println!("end:      {}", end);
+        let mut output = proc_macro2::TokenStream::new();
+        if self.inclusive {
+            for value in start..=end {
+                output.extend(self.inner_expand(value, self.body.clone()));
+            }
+        } else {
+            for value in start..end {
+                output.extend(self.inner_expand(value, self.body.clone()));
+            }
+        }
+        println!("{:#?}", output);
+        syn::Result::Ok(output)
+    }
+
+    fn inner_expand(
+        &self,
+        value: usize,
+        tokens: proc_macro2::TokenStream,
+    ) -> syn::Result<proc_macro2::TokenStream> {
+        let mut tokens: Vec<proc_macro2::TokenTree> = tokens.into_iter().collect();
+        let mut i = 0;
+        while i < tokens.len() {
+            if let proc_macro2::TokenTree::Group(group) = &mut tokens[i] {
+                // process the group as a standalone stream
+                let content = self.inner_expand(value, group.stream())?;
+                // save the original
+                let original_span = group.span();
+                // replace the group which yeilded the recursive call
+                // with a new group built from the previous one
+                *group = proc_macro2::Group::new(group.delimiter(), content);
+                // since group is now the new group
+                // replace the span with the original one
+                group.set_span(original_span);
+                // advance the iteration for the next token
+                i += 1;
+                // skip processing of other possibilities
+                continue;
+            }
+            if let proc_macro2::TokenTree::Ident(ident) = &mut tokens[i] {
+                if *ident == self.var {
+                    let mut lit = proc_macro2::Literal::usize_unsuffixed(value);
+                    lit.set_span(ident.span());
+                    tokens[i] = proc_macro2::TokenTree::Literal(lit);
+                }
+                i += 1;
+                continue;
+            }
+            i += 1;
+        }
+        // at least ironic that we are implementing a mechanism
+        // while using the same kind of mechanism
+        syn::Result::Ok(quote!(#(#tokens)*))
+    }
+}
+
 impl Parse for Seq {
     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
-        let name = input.parse()?;
+        let var = input.parse()?;
         input.parse::<Token![in]>()?;
         let start = input.parse()?;
         let inclusive = if input.peek(Token![..=]) {
@@ -30,36 +103,11 @@ impl Parse for Seq {
         braced!(content in input);
         let body = content.parse()?;
         Ok(Seq {
-            name,
+            var,
             start,
             end,
             body,
             inclusive,
         })
     }
-}
-
-impl Into<proc_macro2::TokenStream> for Seq {
-    fn into(self) -> proc_macro2::TokenStream {
-        self.expand()
-    }
-}
-
-impl Seq {
-    fn expand(&self) -> proc_macro2::TokenStream {
-        self.replace_stream(self.body.clone())
-    }
-
-    fn replace_stream(&self, stream: proc_macro2::TokenStream) -> proc_macro2::TokenStream {
-        // println!("{:#?}", stream.into());
-        quote!()
-    }
-}
-
-#[proc_macro]
-pub fn seq(input: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(input as Seq);
-    let output: proc_macro2::TokenStream = input.into();
-    // output.into()
-    quote!().into()
 }
